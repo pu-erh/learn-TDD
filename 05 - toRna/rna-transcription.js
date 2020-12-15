3ec9c7c1826d732ac344120e7ba17c9727831051
Makiboto <makiboto@gmail.com>
2020-12-15 08:46:32 +0100

    Update rna-transcription.js

--- 05 - toRna/rna-transcription.js
+++ 05 - toRna/rna-transcription.js
@@ -1,21 +1,3 @@
-import {validNucleotide} from '../validNucleotide'
-const TRANSLATIONS = {
-  'G': 'C',
-  'C': 'G',
-  'T': 'A',
-  'A': 'U',
-};
-
 export function toRna(sequence) {
-  let rnaSequence = "";
-
-  for (let i = 0; i < sequence.length; i++) {
-    let nucleotide = sequence[i];
-    if (!validNucleotide(nucleotide)) {
-      throw new Error('Invalid input DNA.');
-    }
-    rnaSequence += TRANSLATIONS[nucleotide];
-  }
-
-  return rnaSequence;
+ 
 }
