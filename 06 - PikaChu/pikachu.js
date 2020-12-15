85b8a03d8e88ad2f3dbf5df7db04e3ba48ccf43f
Makiboto <makiboto@gmail.com>
2020-12-15 08:41:36 +0100

    Update pikachu.js

--- 06 - PikaChu/pikachu.js
+++ 06 - PikaChu/pikachu.js
@@ -1,18 +1,2 @@
 export const pikachu = function (number) {
-  let a = number % 3 === 0;
-  let b = number % 5 === 0;
-
-  if (a) {
-    if (b) {
-      return "PikaChu"
-    }
-
-    return "Pika";
-  } else {
-    if (b) {
-      return "Chu"
-    }
-
-    return number;
-  }
 };
