5cebf2d118597f49cd766ddfca87c9db0704f0e4
Makiboto <makiboto@gmail.com>
2020-12-15 08:47:12 +0100

    Update reverse-string.js

--- 02 - Reverse String/reverse-string.js
+++ 02 - Reverse String/reverse-string.js
@@ -1,13 +1,3 @@
 export function reverseString(string) {
-  if (typeof string != "string") {
-    throw new Error("Please submit a String");
-  }
-
-  let reversedString = "";
-
-  for (let i = string.length; i > 0; i--)
-    reversedString += string[i - 1];
-
-  return reversedString
 }
 
